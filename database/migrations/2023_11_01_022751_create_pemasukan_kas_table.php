99a9c04a1776d6b65dc37a746a2c1924772b2b37
rismaaul <1rismaauliaputri@gmail.com>
2023-11-01 11:52:17 +0700

    third commit

--- database/migrations/2023_11_01_022751_create_pemasukan_kas_table.php
+++ database/migrations/2023_11_01_022751_create_pemasukan_kas_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('pemasukan_kas', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('kode_pemasukan',50)
+            ->nullable(false);
+            $table->string('email_user',50)
+            ->nullable(false);
+            $table->unsignedBigInteger('id_kategori_pemasukan')
+            ->nullable(false);
+            $table->dateTime('tanggal_pemasukan')
+            ->default('2023-01-01 00:00:00')->nullable(false);
+            $table->integer('jumlah_pemasukan')->nullable(false);
         });
     }
 
--- database/migrations/2023_11_01_022852_create_kategori_pengeluarans_table.php
+++ database/migrations/2023_11_01_022852_create_kategori_pengeluarans_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('kategori_pengeluaran', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('jenis_pengeluaran', 40);
         });
     }
 
