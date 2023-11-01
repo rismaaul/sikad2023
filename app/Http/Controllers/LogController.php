32b7de4e8a70f406693fb4ca4f84850ef438e183
rismaaul <1rismaauliaputri@gmail.com>
2023-11-01 11:59:34 +0700

    log commit

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -2,64 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\log;
-use Illuminate\Http\Request;
+use App\Models\Log;
+use Illuminate\View\View;
 
 class LogController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(): View
     {
-        //
-    }
+        $data = [
+            'logs' => Log::query()->orderBy('created_at', 'DESC')->get()
+        ];
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(log $log)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(log $log)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, log $log)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(log $log)
-    {
-        //
+        return view('dashboard.log.index', $data);
     }
 }
